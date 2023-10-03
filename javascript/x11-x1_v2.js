59db0f0222a897039c9a548d44108b694a227b9b
olga232323 <olgaclementemolina@gmail.com>
2023-10-03 20:40:52 +0200

    comentarios varios

--- javascript/x11-x1_v2.js
+++ javascript/x11-x1_v2.js
@@ -1,16 +1,16 @@
 function descodificar_x11(x11) {
     var x1 = '';
   
-    if(x11.length %2 == 0){ // verificar si la cadena de caracteres es impar o no
-        var posicionCaracter = x11.length -1;
+    if(x11.length %2 == 0){ // verificar si la cadena de caracteres es impar o no, se recoje la posicion del ultimo caracter
+        var posicionUltimoCaracter = x11.length -1;
     }else{
-        var posicionCaracter = x11.length -2;
+        var posicionUltimoCaracter = x11.length -2;
     }
     var x1='';
     for (let i = 0; i < x11.length; i+=2) {
         x1+=x11[i];
     }
-    for (let i = posicionCaracter; i >= 0; i-=2) {
+    for (let i = posicionUltimoCaracter; i >= 0; i-=2) {
         x1+=x11[i];
     }
     return x1;
