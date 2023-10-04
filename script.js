d9a35a2a6bf81c2ebb07b0d6a7da3ea89982e289
olga232323 <olgaclementemolina@gmail.com>
2023-10-04 19:41:52 +0200

    Limpiar lineas comentadas + corrección desencriptar/descodificar

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
-// // descodificación x'
-// function descodificar_x1() {
-//     const inputElement = document.getElementById("desencriptarx1");
-//     const resultadoElement = document.getElementById("resultadox1");
-//     const x1 = inputElement.value;
-
-//     var x = '';
-//     var j = 0;
-//     var caracteresCoincidentes = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
-
-//     while (j < x1.length) {
-//         if (!caracteresCoincidentes.includes(x1[j])) { //el caracter (j) que esta leyendo no es vocal (no esta en caracteresCoincidentes)
-//             let sucesion = '';
-//             while (j < x1.length && !caracteresCoincidentes.includes(x1[j])) {
-//                 sucesion += x1[j]; // crea la cadena sucesion sumando el caracter que no es vocal
-//                 j++;
-//             }
-//             x += sucesion.split('').reverse().join(''); // Invertir la sucesión split separa los caracteres, reverse les da la vuelta en espejo y join los vuelve a juntar.
-//         } else {
-//             x += x1[j]; // el caracter (j) que esta leyendo es vocal
-//             j++; // suma el caracter j a la array x
-//         }
-//     }
-//     resultadoElement.textContent = x;
-// }
-
-// descodificación x''
-function descodificar_x11() {
+// Desencriptar x''
+function desencriptar_x11() {
     const inputElement = document.getElementById("desencriptarx11");
     const resultadoElement = document.getElementById("resultadox");
     const x11 = inputElement.value;
